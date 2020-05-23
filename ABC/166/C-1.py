8b1a75b426ee04037ef9793682314915aac19b65
kanekyo1234 <kanekyohunter.0314@softbank.ne.jp>
2020-05-24 03:03:52 +0900

    solve

---
+++ ABC/166/C-1.py
@@ -0,0 +1,12 @@
+n, m = map(int,input().split())
+h = list(map(int,input().split()))
+n_ans=[1]*n
+
+for i in range(m):
+    a, b = map(int,input().split())
+    if h[a-1] <= h[b-1] and n_ans[a-1] == 1:
+        n_ans[a-1] = 0
+    if h[b-1] <= h[a-1] and n_ans[b-1] == 1:
+        n_ans[b-1] = 0
+print(sum(n_ans))
+
